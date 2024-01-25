f58a30ff63e3b8c565b0afad10bd541d2a5a2148
Gennaro17091810 <157720536+Gennaro17091810@users.noreply.github.com>
2024-01-25 15:57:36 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["🤖BOT UFFICIALE LOS SANTOS RP"];
 
 
 let currentIndex = 0;
